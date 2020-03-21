ee4aafc2574efd19bf25b186d17b267194b4c106
Merijn Hendriks <innohurrytocode@gmail.com>
2020-03-21 04:00:10 +0100

    [WIP} work dump

--- EmuLib/Hooks/Loader.cs
+++ EmuLib/Hooks/Loader.cs
@@ -10,11 +10,13 @@ namespace EFT.Trainer
 			get
 			{
 				var result = GameObject.Find("Application (Main Client)");
+
 				if (result == null)
 				{
 					result = new GameObject("EmuInstance");
 					Object.DontDestroyOnLoad(result);
 				}
+
 				return result;
 			}
 		}
